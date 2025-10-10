9eb33ba02b8c1a0db60ef05c5a336b660bdd01a2
ZeroDream <140892023+zero-dream@users.noreply.github.com>
2025-10-10 14:26:06 +0800

    DeleteFixed nss-firmware hash

--- script/FirmwareHandle.sh
+++ script/FirmwareHandle.sh
@@ -81,13 +81,13 @@ fi
 # --------------------------------------------------
 
 # 临时修复 nss-firmware 校验不通过
-cd "$pkgPath"
-NSS_FIRMWARE_FILE="$WRT_MainPath/feeds/nss_packages/firmware/nss-firmware/Makefile"
-if [ -f "$NSS_FIRMWARE_FILE" ]; then
-	sed -i 's/3ec87f221e8905d4b6b8b3d207b7f7c4666c3bc8db7c1f06d4ae2e78f863b8f4/881cbf75efafe380b5adc91bfb1f68add5e29c9274eb950bb1e815c7a3622807/g' "$NSS_FIRMWARE_FILE"
-	echo 'Fixed: nss-firmware'
-	echo ''
-fi
+# cd "$pkgPath"
+# NSS_FIRMWARE_FILE="$WRT_MainPath/feeds/nss_packages/firmware/nss-firmware/Makefile"
+# if [ -f "$NSS_FIRMWARE_FILE" ]; then
+# 	sed -i 's/3ec87f221e8905d4b6b8b3d207b7f7c4666c3bc8db7c1f06d4ae2e78f863b8f4/881cbf75efafe380b5adc91bfb1f68add5e29c9274eb950bb1e815c7a3622807/g' "$NSS_FIRMWARE_FILE"
+# 	echo 'Fixed: nss-firmware'
+# 	echo ''
+# fi
 
 # --------------------------------------------------
 
