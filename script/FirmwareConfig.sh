45f97fb66a26dfda6356254ab5500f565f50236b
ZeroDream <140892023+zero-dream@users.noreply.github.com>
2025-12-03 17:51:31 +0800

    Update FirmwareConfig.sh

--- script/FirmwareConfig.sh
+++ script/FirmwareConfig.sh
@@ -45,8 +45,12 @@ if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 	echo "CONFIG_PACKAGE_luci-app-sqm=y" >>"$WRT_ConfigPath"
 	echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >>"$WRT_ConfigPath"
 	# 设置 NSS 版本
-	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >>"$WRT_ConfigPath"
-	echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >>"$WRT_ConfigPath"
+	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> "$WRT_ConfigPath"
+	if [[ "${WRT_CONFIG,,}" == *"ipq50"* ]]; then
+		echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >>"$WRT_ConfigPath"
+	else
+		echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >>"$WRT_ConfigPath"
+	fi
   # 添加配置成功
   echo 'Successful: addQualcommaxConfig'
 	echo ''
